70be5a9301512b91fe2659b04bb1436fd760c1fb
RDarkCat <45436866+RDarkCat@users.noreply.github.com>
2020-08-06 23:17:32 +0300

    Update CategoryController.php
    Анти-паттерн "Магические числа"

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,11 +7,14 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
+    const STATUS_PAGE_NOT_FOUND = 404;
+    
     public function __invoke(string $name)
     {
         $category = Category::where('name', $name)->first();
+
         if (!$category) {
-            return abort(404);
+            return abort(STATUS_PAGE_NOT_FOUND);
         }
 
         return view('categories.show', ['category' => $category]);
